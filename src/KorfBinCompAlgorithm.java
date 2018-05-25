1fd7d9f5a7e21dc56cdc7be6fa9da7df58fc5a56
Koenraad Drost <s1123524@student.windesheim.nl>
2018-05-25 12:06:38 +0200

    Korf is volledig klaar.

--- src/KorfBinCompAlgorithm.java
+++ src/KorfBinCompAlgorithm.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.lang.Math;
 
 public class KorfBinCompAlgorithm implements BPPAlgorithm {
     private BPPAlgorithm algorithmCall; //for calling best fit in Korf's
     private BPPAlgorithm optimalSolution;
     private List<Container> containers;
     private List<Box> boxes;
+
+    private List<Container> startSolution;
+    private int startContainersCount;
+    private List<Container> optimalArray;
+    private int optimalContainers = 0;
+    private List<Container> workingSolution;
+
     private long solveTime = 0;
 
     static final String NAME = "KorfBinCompletion";
-
-    private int optimalContainers = 0;
-    private int optimalRest = 0; //TODO: echt nodig?
-    private int totalBoxHeight = 0;
-    private int containerCount = 0;
 
 
     KorfBinCompAlgorithm(List<Container> containers, List<Box> boxes) {
@@ -25,39 +25,58 @@ public class KorfBinCompAlgorithm implements BPPAlgorithm {
         this.boxes = boxes;
     }
 
-    public List<Container> solveSteps(int steps){
-        List<Container> startSolution;
-        int startContainersCount;
+    public List<Container> solveSteps(int steps) {
         long startTime = System.nanoTime();
 
         //Order boxes by size.
-        Collections.sort(boxes,(a,b) -> b.compareTo(a));
+        Collections.sort(this.boxes, (a, b) -> b.compareTo(a));
 
         // Innitial run of Best Fit as starting solution to be optimized.
-        this.algorithmCall = new BestFitAlgorithm(this.containers,this.boxes);
-        startSolution = algorithmCall.getContainers();
-        startContainersCount = algorithmCall.getContainerCount();
+        this.algorithmCall = new BestFitAlgorithm(this.containers, this.boxes);
+        this.startSolution = algorithmCall.getContainers();
+        this.startContainersCount = startSolution.size();
 
-        //Again to get a duplicate which i can safely chop up boxes in.
-        this.optimalSolution = new BestFitAlgorithm(this.containers,this.boxes);
-        List<Container> optimalArray = optimalSolution.getContainers();
+        //Again to get a duplicate where i can safely chop-up boxes in.
+        this.optimalSolution = new BestFitAlgorithm(this.containers, this.boxes);
+        this.optimalArray = optimalSolution.getContainers();
         this.optimalContainers = optimalArray.size();
+
+        //removing empty containers:
+        Iterator<Container> itStart = startSolution.iterator();
+
+        while (itStart.hasNext()) {
+            if (itStart.next().getPercentageFilled() < 1) {
+                itStart.remove();
+            }
+        }
+
+        Iterator<Container> itOptimal = optimalArray.iterator();
+
+        while (itOptimal.hasNext()) {
+            if (itOptimal.next().getPercentageFilled() < 1) {
+                itOptimal.remove();
+            }
+        }
+
+        this.workingSolution = startSolution;
+        this.startContainersCount = startSolution.size();
+        this.optimalContainers = optimalArray.size();
+
 
         //Determine optimal containers to achieve with algorithm.(Boxes can be changed in height here)
 
-        //TODO: set " 5 " to correct max.
+        // -2 because the last container has no options for optimization
+        for (int iSta = 0; iSta < optimalContainers - 2; iSta++) {
 
-        for(int iSta = 0; iSta < optimalArray.size()-2; iSta++ ) {
-
-            int iRest = 100 - optimalArray.get(iSta).getPercentageFilled();
-            ArrayList<Box> altBoxes = new ArrayList<>();
+            int iRest = 100 - this.optimalArray.get(iSta).getPercentageFilled();
+            List<Box> altBoxes = new ArrayList<>();
             List<Box> restSolveList = new ArrayList<>();
 
-            List<Box> cOboxes = optimalArray.get(iSta+1).getBoxes();
-            for(int iOAC = 0; iOAC < cOboxes.size()-1; iOAC++){
-                int cBHeight = optimalArray.get(iSta+1).getBox(iOAC).getHeight();
-                if(cBHeight < iRest){
-                    altBoxes.add(optimalArray.get(iSta+1).getBox(iOAC));
+            for (int iStaB = iSta + 1; iStaB < optimalContainers - 1; iStaB++) {
+                for (Box b : optimalArray.get(iStaB).getBoxes()) {
+                    if (b.getHeight() <= iRest) {
+                        altBoxes.add(b);
+                    }
                 }
             }
 
@@ -68,7 +87,6 @@ public class KorfBinCompAlgorithm implements BPPAlgorithm {
                 for (int iAlt = 0; iAlt < altBoxes.size() - 1; iAlt++) {
                     if (altBoxes.get(iAlt).getHeight() < iRest) {
                         restSolveList.add(altBoxes.get(iAlt));
-                        altBoxes.remove(altBoxes.get(iAlt));
                         iRest -= altBoxes.get(iAlt).getHeight();
                     } else if (iRest > 0 && restSolveList.size() > 0 && altBoxes.size() > 0) {
                         int x = altBoxes.get(iAlt).getHeight() - iRest;
@@ -81,68 +99,100 @@ public class KorfBinCompAlgorithm implements BPPAlgorithm {
                 }
             }
 
-            //Rearranging optimalArray so that the solution gets implemented.
-            for (int iRL = 0; iRL < restSolveList.size()-1; iRL++) {
-                optimalArray.get(iSta).addBox(restSolveList.get(iRL));
-                optimalArray.get(iSta+1).removeBox(restSolveList.get(iRL));
-            }
+            //Rearranging optimalArray so that the optimalSolution gets implemented.
+            if (restSolveList.size() > 0 && restSolveList != null) {
+                Box y = optimalArray.get(0).getBox(0);
+                int resolveIndex = 0;
+                Box sbox = restSolveList.get(resolveIndex);
 
-            //removing empty containers from OptimalArray.
-            if(optimalArray.size() > 0) {
-                for (int cI = 0; cI < optimalArray.size()-1; cI++) {
-                    int fill = optimalArray.get(cI).getBoxes().size();
-                    if (fill < 1) {
-                        optimalArray.remove(cI);
+                Iterator<Box> itRSolve = restSolveList.iterator();
+
+                while (itRSolve.hasNext()) {
+                    y = itRSolve.next();
+                    Container z = optimalArray.get(0);
+                    for (Container c : optimalArray) {
+                        for (Box b : c.getBoxes()) {
+                            if (b == sbox) {
+                                z = c;
+                            }
+                        }
+                    }
+                    if (z != optimalArray.get(0)) {
+                        Iterator<Box> iRCB = z.getBoxes().iterator();
+
+                        while (iRCB.hasNext()) {
+                            if (iRCB.next() == y && y != optimalArray.get(0).getBox(0)) {
+                                iRCB.remove();
+                            }
+                        }
+                    }
+                    System.out.println(" restsolve.indes : " + restSolveList.get(resolveIndex));
+                    this.optimalArray.get(iSta).addBox(restSolveList.get(resolveIndex));
+                    resolveIndex++;
+                }
+
+            }
+        }
+
+        //removing empty containers from OptimalArray.
+        Iterator<Container> itOptFinal = optimalArray.iterator();
+
+        while (itOptFinal.hasNext()) {
+            if (itOptFinal.next().getPercentageFilled() < 1) {
+                itOptFinal.remove();
+            }
+        }
+        this.optimalContainers = this.optimalArray.size();
+
+
+        //Repetitive Best Fit runs to get best solution.
+        while (workingSolution.size() > optimalContainers) {
+            int workingSolSize = workingSolution.size();
+
+            // -2 because the last container has no options for optimization.
+            for (int iCS = 0; iCS < workingSolSize - 2; iCS++) {
+
+                int currentRest = 100 - workingSolution.get(iCS).getPercentageFilled();
+                int containerExchangeIndex = -1;
+
+                Iterator<Container> itWSolution = workingSolution.iterator();
+
+                int WSolContInd = iCS;
+
+                while (itWSolution.hasNext()) {
+
+                    Container WSCont = workingSolution.get(WSolContInd);
+
+                    Iterator<Box> itWScontBoxes = WSCont.getBoxes().iterator();
+
+                    while (itWScontBoxes.hasNext()) {
+                        if (itWScontBoxes.next().getHeight() <= currentRest) {
+                            workingSolution.get(iCS).addBox(itWScontBoxes.next());
+                            itWSolution.next().removeBox(itWScontBoxes.next());
+                        }
+
+                    }
+
+                    WSolContInd++;
+                }
+                //removing empty containers from workingSolution.
+                Iterator<Container> itWrkFinal = workingSolution.iterator();
+
+                while (itWrkFinal.hasNext()) {
+                    if (itWrkFinal.next().getPercentageFilled() < 1) {
+                        itWrkFinal.remove();
                     }
                 }
+
             }
-
-
-            this.optimalContainers = optimalArray.size();
-            System.out.println("optimalContainers: " + optimalContainers);
-            System.out.println("Optimal Array: " + optimalArray);
-
-
-            //Repetitive Best Fit runs to get optimal solution.
-
-        /*
-
-        List<Container> solution = Stream.generate(Container::new).limit(this.containerCount).collect(Collectors.toList());
-        int cursor = 0;
-        int t = 0;
-
-        for (Box b : this.boxes) {
-            int h = 100;
-            int g = 0;
-            for (Container c : solution){
-                g = solution.get(cursor).getPercentageFilled() + b.getHeight();
-                if(h > g){
-                    h = g;
-                    t = cursor;
-                }
-                cursor += 1;
-            }
-            if (h == 100){
-                solution.add(new Container());
-                t = cursor;
-            }
-            solution.get(t).addBox(b);
-            cursor = 0;
-            t = 0;
         }
 
-        */
-
-        }
 
         this.solveTime = System.nanoTime() - startTime;
-        return algorithmCall.getContainers(); //TODO remove temp test fix
+        return workingSolution;
 
 
     }
-
-
-
 
 
     public List<Container> getContainers() {
